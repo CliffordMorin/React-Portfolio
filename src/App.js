b330c77e5c497a9eb2d5ee8caeda0480c5414f06
Clifford Morin <cemorin21@gmail.com>
2021-06-22 14:22:29 -0400

    7. added react dom router and routed deperate pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 //Global Style
-import GlobalStyle from './components/GlobalStyle'
+import GlobalStyle from "./components/GlobalStyle";
 
 //Import Pages
-import AboutUs from './pages/AboutUs'
-import Nav from './components/Nav'
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-     <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>Our Contact</h1>
+        </div>
+    );
+};
+
+export default ContactUs;

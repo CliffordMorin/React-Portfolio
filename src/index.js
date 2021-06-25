8ca9fea151625444f6667bdf49e146a4b4728fe1
Clifford Morin <cemorin21@gmail.com>
2021-06-24 20:22:47 -0400

    changed svg sizing and updated to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,69 +1,111 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 //Images
-import athlete from '../img/athlete-small.png';
-import theracer from '../img/theracer-small.png';
-import goodtimes from '../img/goodtimes-small.png';
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 //Animation
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import {
+  pageAnimation,
+  fadeAnim,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation";
 
 const OurWork = () => {
-    return (
-        <StyledWork 
-            variants={pageAnimation} 
-            exit="exit" 
-            initial="hidden" 
-            animate="show"
-            style={{ background: "#fff" }}
-        >
-                <StyledMovie>
-                    <h2>The Athlete</h2>
-                    <div className="line"></div>
-                    <Link to="/work/the-athlete">
-                    <img src={athlete} alt="athlete" />
-                    </Link>
-                </StyledMovie>
-                <StyledMovie>
-                    <h2>The Racer</h2>
-                    <div className="line"></div>
-                    <Link to="/work/the-racer">
-                    <img src={theracer} alt="theracer" />
-                    </Link>
-                </StyledMovie>
-                <StyledMovie>
-                    <h2>Good Times</h2>
-                    <div className="line"></div>
-                    <Link to="/work/good-times">
-                    <img src={goodtimes} alt="good-times" />
-                    </Link>
-                </StyledMovie>
-        </StyledWork>
-    );
+  return (
+    <StyledWork
+      variants={pageAnimation}
+      exit="exit"
+      initial="hidden"
+      animate="show"
+      style={{ background: "#fff" }}
+    >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
+      <StyledMovie>
+        <motion.h2 variants={fadeAnim}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
+        <Link to="/work/the-athlete">
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+          </Hide>
+        </Link>
+      </StyledMovie>
+      <StyledMovie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="theracer" />
+        </Link>
+      </StyledMovie>
+      <StyledMovie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link to="/work/good-times">
+          <img src={goodtimes} alt="good-times" />
+        </Link>
+      </StyledMovie>
+    </StyledWork>
+  );
 };
 
 const StyledWork = styled(motion.div)`
-    min-height: 100vh;
-    overflow: hidden;
-    padding: 5rem 10rem;
-    h2{
-        padding: 1rem 0rem;
-    }
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
 `;
 
 const StyledMovie = styled.div`
-    padding-bottom: 10rem;
-    .line{
-        height: 0.5rem;
-        background: #cccccc;
-        margin-bottom: 3rem;
-    }
-    img{
-        width: 100%;
-        height: 70vh;
-        object-fit: cover;
-    }
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #23d997;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+  background: #f4ccf4;
+`;
+
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork;

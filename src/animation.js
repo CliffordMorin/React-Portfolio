550ceab2847a4d34780b3134dadfb4c081d79cf5
Clifford Morin <cemorin21@gmail.com>
2021-07-28 02:36:05 -0400

    fixed side scroll, on iphone by changing scale in scrolreveal so it goes from smaller to larger instead of larger to smaller

--- src/animation.js
+++ src/animation.js
@@ -66,7 +66,7 @@ export const sliderContainer = {
 };
 
 export const scrollReveal = {
-  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: .75, transition: { duration: 0.5 } },
   show: {
     opacity: 1,
     scale: 1,
